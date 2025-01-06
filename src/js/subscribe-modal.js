f726e22a8d204c624e31418bcc34582481601498
elen-purple <anna.havrylova.2011@gmail.com>
2025-01-06 12:18:56 +0200

    Update subscribe-modal.js

--- src/js/subscribe-modal.js
+++ src/js/subscribe-modal.js
@@ -3,16 +3,20 @@ const backdrop = document.querySelector(`[data-subscribe="backdrop"]`);
 const closeBtn = document.querySelector(`[data-subscribe="close"]`);
 const form = document.querySelector('#subscribe-form');
 
-form.addEventListener('submit', toggleModal);
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  e.currentTarget.querySelector('input').value = '';
+  page.classList.toggle('no-scroll');
+  backdrop.classList.toggle('is-hidden');
+});
 closeBtn.addEventListener('click', toggleModal);
 backdrop.addEventListener('click', toggleModal);
 
 function toggleModal(e) {
-    e.preventDefault();
+  e.preventDefault();
   if (e.currentTarget === backdrop && e.currentTarget !== e.target) {
     return;
   }
   page.classList.toggle('no-scroll');
   backdrop.classList.toggle('is-hidden');
 }
-
