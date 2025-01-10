01b61d47a3cd667c4506761d74dc285d76a2679a
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-01-11 00:13:24 +0200

    full js time

---
+++ src/js/time-calculator.js
@@ -0,0 +1,30 @@
+const inputElements = document.querySelector("#input");
+const buttonElements = document.querySelector("#button");
+const textElements = document.querySelector("#text");
+
+buttonElements.addEventListener("click", (events) => {
+
+    events.preventDefault();
+
+    let days = Math.floor(Number(inputElements.value) / 1440);
+    let hours = Math.floor((Number(inputElements.value) - days * 1440) / 60);
+    let min = Number(inputElements.value) - days * 1440 - hours * 60
+
+    if (hours < 12) {
+        hours = `${hours}`;
+    }
+
+    if (min < 60) {
+        min = `${min}`;
+    }
+
+    if (days <= 0) {
+        textElements.textContent = `${hours}:${min}`;
+    } else {
+        textElements.textContent = `${days} дн. ${hours}:${min}`;
+    }
+
+
+    inputElements.value = "";
+
+});
