480a102763ea3efb11ee3d0914d3f279e06cd49e
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-01-11 14:37:56 +0200

    fix time and start slider

---
+++ src/js/team-slider.js
@@ -0,0 +1,6 @@
+const list = document.querySelector('#list');
+const items = document.querySelectorAll('#item');
+const leftButton = document.querySelector('#left');
+const rightButton = document.querySelector('#right');
+
+let index = 0;
