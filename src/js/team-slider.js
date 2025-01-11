e2810f0af45e67a621c62f75b4c2aceaba7d3289
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-01-12 00:07:49 +0200

    js-slider

--- src/js/team-slider.js
+++ src/js/team-slider.js
@@ -1,6 +1,25 @@
-const list = document.querySelector('#list');
-const items = document.querySelectorAll('#item');
-const leftButton = document.querySelector('#left');
-const rightButton = document.querySelector('#right');
 
-let index = 0;
+    const items = document.querySelectorAll('#item');
+    const leftButton = document.getElementById('left');
+    const rightButton = document.getElementById('right');
+
+    let basicIndex = 0;
+
+    function updateSlider() {
+        items.forEach((items, index) => {
+            items.style.display = index === basicIndex ? 'block' : 'none';
+        });
+    }
+
+    leftButton.addEventListener('click', function () {
+        basicIndex = (basicIndex > 0) ? basicIndex - 1 : items.length - 1;
+        updateSlider();
+    });
+
+    rightButton.addEventListener('click', function () {
+        basicIndex = (basicIndex < items.length - 1) ? basicIndex + 1 : 0;
+        updateSlider();
+    });
+
+
+
